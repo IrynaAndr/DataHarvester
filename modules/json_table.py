ea1598335434318a4692191abf082a2c95811d17
IrynaAndr <63851046+IrynaAndr@users.noreply.github.com>
2024-01-23 18:46:23 +0100

    Update json_table.py

--- modules/json_table.py
+++ modules/json_table.py
@@ -8,7 +8,6 @@ def get_class_names(url):
     try:
         
         response = requests.get(url)
-
         if response.status_code == 200:
             
             soup = BeautifulSoup(response.text, 'html.parser')
@@ -17,22 +16,18 @@ def get_class_names(url):
                 # Find all table rows (excluding the header row if applicable)
                 rows = table.find_all('tr')[1:]  # Exclude header row if present
 
-                # Extract class names from the first row
+                # getting class names inside tr as 'key' for json 
                 first_row = rows[0] if rows else None
                 class_names = [cell['class'][0] for cell in first_row.find_all(['td', 'th'])] if first_row else []
 
                 return class_names
             else:
-                print('Table not found on the page.')
                 return 'Table not found on the page.'
         else:
-            msg = f"Failed to retrieve the webpage. Status code: {response.status_code}"
-            print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-            return msg
+            return f"Failed to retrieve the webpage. Status code: {response.status_code}"
 
     except requests.RequestException as e:
-        msg = f"Error during request: {e}"
-        return msg
+        return f"Error during request: {e}"
 
 def save_data_to_json(url):
     try:
@@ -40,36 +35,22 @@ def save_data_to_json(url):
         class_names = get_class_names(url)
 
         if class_names:
-            # Send a GET request to the URL
             response = requests.get(url)
-
             if response.status_code == 200:
                 
                 soup = BeautifulSoup(response.text, 'html.parser')
                 table = soup.find('table')
                 if table:
-                    # Initialize a list to store dictionaries for each row
                     data_list = []
-
-                    # Find all table rows (excluding the header row if applicable)
                     rows = table.find_all('tr')[1:]  # Exclude header row if present
-
-                    # Iterate through rows and extract data
                     for row in rows:
-                        # Extract text from each cell in the row
+                        
                         cells = [cell.get_text(strip=True) for cell in row.find_all(['td', 'th'])]
-
-                        # Create a dictionary for the row
                         row_dict = dict(zip(class_names, cells))
-
-                        # Append the row dictionary to the list
                         data_list.append(row_dict)
 
-                    # Extract the filename from the URL
                     url_parsed = urlparse(url)
                     filename = url_parsed.netloc + url_parsed.path.replace('/', '_') + '_table_data.json'
-
-                    # Get the path to the Downloads folder
                     downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
 
                     # Save the data to a JSON file with the derived filename in the Downloads folder
@@ -77,21 +58,16 @@ def save_data_to_json(url):
                     with open(json_file_path, 'w', encoding='utf-8') as jsonfile:
                         json.dump(data_list, jsonfile, indent=2)
 
-                    msg = f'JSON file created successfully. Saved to: {json_file_path}'
-                    return msg
+                    
+                    return f'JSON file created successfully. Saved to: {json_file_path}'
                 else:
-                    msg = 'Table not found on the page.'
-                    return msg
+                    return 'Table not found on the page.'
             else:
-                msg = f"Failed to retrieve the webpage. Status code: {response.status_code}"
-                return msg
+                return f"Failed to retrieve the webpage. Status code: {response.status_code}"
     except Exception as e:
-        msg = f"An error occurred: {e}"
-        return msg
+        return f"An error occurred: {e}"
 
-# Replace this URL with the actual URL of the webpage containing the table
-url = 'https://en.wikipedia.org/wiki/Table_(information)'
 
-# Call the function to save data to JSON
+
+url = 'https://www.scrapethissite.com/pages/forms/'
 #print(save_data_to_json(url))
-save_data_to_json(url)
